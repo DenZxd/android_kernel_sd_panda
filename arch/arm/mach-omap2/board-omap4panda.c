0644748431ec954513085d17276e2f28f5ccf65b
MeiHui FAN <meihui.fan@gmail.com>
2011-12-25 18:32:41 +0800

    add SD card detection IRQ for pandaboard
    Change-Id: I73a21521d12c7abe8e31dbdfee34e6fb80ea51bd

--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -265,8 +265,11 @@ static int omap4_twl6030_hsmmc_late_init(struct device *dev)
 		 if (ret)
 			dev_err(dev, "%s: Error card detect config(%d)\n",
 				__func__, ret);
-		 else
+		 else {
 			pdata->slots[0].card_detect = twl6030_mmc_card_detect;
+			pdata->slots[0].card_detect_irq = TWL6030_IRQ_BASE +
+				MMCDETECT_INTR_OFFSET;
+		 }
 	}
 	return ret;
 }
