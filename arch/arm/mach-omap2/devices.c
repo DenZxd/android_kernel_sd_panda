b97d5faaebec8362741319d0e40c8901c523a18b
Sergio Aguirre <saaguirre@ti.com>
2012-02-17 17:01:31 -0600

    OMAP4: Fix use of omap_device_build
    Signed-off-by: Sergio Aguirre <saaguirre@ti.com>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -227,7 +227,7 @@ int omap3_init_camera(struct isp_platform_data *pdata)
 
 int omap4_init_camera(struct iss_platform_data *pdata, struct omap_board_data *bdata)
 {
-	struct platform_device *pdev;
+	struct omap_device *od;
 	struct omap_hwmod *oh;
 	struct iss_platform_data *omap4iss_pdata;
 	const char *oh_name = "iss";
@@ -241,13 +241,13 @@ int omap4_init_camera(struct iss_platform_data *pdata, struct omap_board_data *b
 
 	omap4iss_pdata = pdata;
 
-	pdev = omap_device_build(name, -1, oh, omap4iss_pdata,
+	od = omap_device_build(name, -1, oh, omap4iss_pdata,
 			sizeof(struct iss_platform_data), NULL, 0, 0);
 
-	if (IS_ERR(pdev)) {
+	if (IS_ERR(od)) {
 		WARN(1, "Can't build omap_device for %s:%s.\n",
 						name, oh->name);
-		return PTR_ERR(pdev);
+		return PTR_ERR(od);
 	}
 
 	oh->mux = omap_hwmod_mux_init(bdata->pads, bdata->pads_cnt);
