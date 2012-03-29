10e456bb2fe45656a78500c1af6f63c6b014a949
MeiHui FAN <meihui.fan@gmail.com>
2012-03-29 17:36:34 +0800

    set lpj_fine to speed-up boot process
    also fix lpj_fine for SMPs
    
    Change-Id: Iafaee94c5858b247065cd087b32d6022a32a20b3

--- init/calibrate.c
+++ init/calibrate.c
@@ -253,9 +253,9 @@ void __cpuinit calibrate_delay(void)
 		if (!printed)
 			pr_info("Calibrating delay loop (skipped) "
 				"preset value.. ");
-	} else if ((!printed) && lpj_fine) {
+	} else if (lpj_fine) {
 		lpj = lpj_fine;
-		pr_info("Calibrating delay loop (skipped), "
+		if (!printed) pr_info("Calibrating delay loop (skipped), "
 			"value calculated using timer frequency.. ");
 	} else if ((lpj = calibrate_delay_direct()) != 0) {
 		if (!printed)
