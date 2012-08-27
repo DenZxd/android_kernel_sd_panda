1f2f4aec4ca9b5eed2efe5160e80d0fc57d9e145
yaojuntao <yaojuntao@hhcn.com>
2012-08-27 09:47:04 +0800

    use static buf for wlan

--- arch/arm/mach-omap2/board-panda-wifi.c
+++ arch/arm/mach-omap2/board-panda-wifi.c
@@ -42,6 +42,63 @@
 #define WL_RST_N 40
 #define GPIO_WLAN_IRQ	53
 
+#if defined(CONFIG_DHD_USE_STATIC_BUF)
+#define PREALLOC_WLAN_NUMBER_OF_SECTIONS	4
+#define PREALLOC_WLAN_NUMBER_OF_BUFFERS		160
+#define PREALLOC_WLAN_SECTION_HEADER		24
+
+#define WLAN_SECTION_SIZE_0	(PREALLOC_WLAN_NUMBER_OF_BUFFERS * 128)
+#define WLAN_SECTION_SIZE_1	(PREALLOC_WLAN_NUMBER_OF_BUFFERS * 128)
+#define WLAN_SECTION_SIZE_2	(PREALLOC_WLAN_NUMBER_OF_BUFFERS * 512)
+#define WLAN_SECTION_SIZE_3	(PREALLOC_WLAN_NUMBER_OF_BUFFERS * 1024)
+
+#define WLAN_SKB_BUF_NUM	16
+
+static struct sk_buff *wlan_static_skb[WLAN_SKB_BUF_NUM];
+
+typedef struct wifi_mem_prealloc_struct {
+	void *mem_ptr;
+	unsigned long size;
+} wifi_mem_prealloc_t;
+
+static wifi_mem_prealloc_t wifi_mem_array[PREALLOC_WLAN_NUMBER_OF_SECTIONS] = {
+	{ NULL, (WLAN_SECTION_SIZE_0 + PREALLOC_WLAN_SECTION_HEADER) },
+	{ NULL, (WLAN_SECTION_SIZE_1 + PREALLOC_WLAN_SECTION_HEADER) },
+	{ NULL, (WLAN_SECTION_SIZE_2 + PREALLOC_WLAN_SECTION_HEADER) },
+	{ NULL, (WLAN_SECTION_SIZE_3 + PREALLOC_WLAN_SECTION_HEADER) }
+};
+
+static void *panda_wifi_mem_prealloc(int section, unsigned long size)
+{
+	if (section == PREALLOC_WLAN_NUMBER_OF_SECTIONS)
+		return wlan_static_skb;
+	if ((section < 0) || (section > PREALLOC_WLAN_NUMBER_OF_SECTIONS))
+		return NULL;
+	if (wifi_mem_array[section].size < size)
+		return NULL;
+	return wifi_mem_array[section].mem_ptr;
+}
+
+int __init panda_init_wifi_mem(void)
+{
+	int i;
+
+	for(i=0;( i < WLAN_SKB_BUF_NUM );i++) {
+		if (i < (WLAN_SKB_BUF_NUM/2))
+			wlan_static_skb[i] = dev_alloc_skb(4096);
+		else
+			wlan_static_skb[i] = dev_alloc_skb(8192);
+	}
+	for(i=0;( i < PREALLOC_WLAN_NUMBER_OF_SECTIONS );i++) {
+		wifi_mem_array[i].mem_ptr = kmalloc(wifi_mem_array[i].size,
+							GFP_KERNEL);
+		if (wifi_mem_array[i].mem_ptr == NULL)
+			return -ENOMEM;
+	}
+	return 0;
+}
+#endif
+
 static struct resource panda_wifi_resources[] = {
 	[0] = {
 		.name       = "bcmdhd_wlan_irq",
@@ -206,6 +263,9 @@ static struct wifi_platform_data panda_wifi_control = {
 	.set_power      = panda_wifi_power,
 	.set_reset      = panda_wifi_reset,
 	.set_carddetect = panda_wifi_set_carddetect,
+#if defined(CONFIG_DHD_USE_STATIC_BUF)
+	.mem_prealloc	= panda_wifi_mem_prealloc,
+#endif
 	.get_country_code = panda_wifi_get_country_code,
 };
 
@@ -239,6 +299,10 @@ int __init panda_wlan_init(void)
 			__func__, GPIO_WLAN_IRQ);
 		return ret;
 	} else gpio_direction_input(GPIO_WLAN_IRQ);
+
+#if defined(CONFIG_DHD_USE_STATIC_BUF)
+	panda_init_wifi_mem();
+#endif
 
 	return platform_device_register(&panda_wifi_device);
 }
